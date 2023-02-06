b7e126aba8cd513c3ab8e786f194ab2f49d83bb0
JustinM95 <justin.mitchell09@gmail.com>
2023-02-06 17:37:38 -0600

    Removed unused imports

--- src/main/java/com/justinmtech/webank/Webank.java
+++ src/main/java/com/justinmtech/webank/Webank.java
@@ -1,7 +1,5 @@
 package com.justinmtech.webank;
 
-import com.justinmtech.webank.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
